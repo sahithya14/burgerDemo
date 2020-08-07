718704fbc6212f10b8cb058c117511da1f6d56aa
sahithya14 <sahithyareddy33@gmail.com>
2020-08-07 18:53:27 +0530

    Adding burger comp in Burger builder

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,13 +7,13 @@ const burgerIngredient = (props) => {
 
   switch (props.type) {
     case "bread-bottom":
-      ingredient = <div className={clases.breadBottom}></div>;
+      ingredient = <div className={clases.breadBottom}>breadBottom</div>;
       break;
     case "bread-top":
       ingredient = (
         <div className={clases.breadTop}>
-          <div className={clases.Seeds1}></div>
-          <div className={clases.Seeds2}></div>
+          <div className={clases.Seeds1}>breadTop</div>
+          <div className={clases.Seeds2}>breadTop</div>
         </div>
       );
       break;
