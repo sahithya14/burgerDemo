a122424f74b917235381a86b234b28e10cf261d6
sahithya14 <sahithyareddy33@gmail.com>
2020-08-07 11:50:48 +0530

    burger ingredient changes

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,8 +1,37 @@
 import React from "react";
+import clases from "./BurgerIngredient.module.css";
 
-const burgerIngredient = props => {
-  return <div />;
+const burgerIngredient = (props) => {
+  var ingredient = null;
+
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className={clases.breadBottom}></div>;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className={clases.breadTop}>
+          <div className={clases.Seeds1}></div>
+          <div className={clases.Seeds2}></div>
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={clases.meat}></div>;
+      break;
+    case "cheese":
+      ingredient = <div className={clases.cheese}></div>;
+      break;
+    case "bacon":
+      ingredient = <div className={clases.bacon}></div>;
+      break;
+    case "salad":
+      ingredient = <div className={clases.salad}></div>;
+      break;
+    default:
+      ingredient = null;
+  }
+  return ingredient;
 };
 
 export default burgerIngredient;
-    
