37d7c85338c7b2012b352ad51aeb9484b9b8d1bc
sahithya14 <sahithyareddy33@gmail.com>
2020-08-07 17:29:21 +0530

    prop-types module aded

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React from "react";
 import clases from "./BurgerIngredient.module.css";
+import PropTypes from "prop-types";
 
 const burgerIngredient = (props) => {
   var ingredient = null;
@@ -34,4 +35,7 @@ const burgerIngredient = (props) => {
   return ingredient;
 };
 
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
 export default burgerIngredient;
