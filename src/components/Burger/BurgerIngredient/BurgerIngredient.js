99cd2777a5a82c0622016cc37f1ef8178095768b
sahithya14 <sahithyareddy33@gmail.com>
2020-08-07 22:35:57 +0530

    burger styles

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,13 +7,13 @@ const burgerIngredient = (props) => {
 
   switch (props.type) {
     case "bread-bottom":
-      ingredient = <div className={clases.breadBottom}>breadBottom</div>;
+      ingredient = <div className={clases.BreadBottom}></div>;
       break;
     case "bread-top":
       ingredient = (
         <div className={clases.BreadTop}>
-          <div className={clases.Seeds1}>breadTop</div>
-          <div className={clases.Seeds2}>breadTop</div>
+          <div className={clases.Seeds1}></div>
+          <div className={clases.Seeds2}></div>
         </div>
       );
       break;
