d76afc9f3fce111e1b93d4d9b75f0c12b21c4f61
sahithya14 <sahithyareddy33@gmail.com>
2020-08-19 23:40:23 +0530

    Axios  instance createdand added to  burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axiousInstance from "../../hoc/axios-orders";
 const INGRIENDENTS_PRICE = {
   meat: 50.5,
   cheese: 20.1,
@@ -68,7 +69,29 @@ class BurgerBudilder extends Component {
     this.setState({ purchasingOrder: false });
   };
   successPurchasingHandler = () => {
-    alert("success");
+    var order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name1: "sahithya",
+        address: {
+          street: "kukatpally",
+          pinCode: "505474",
+          country: "INDIA"
+        },
+        email: "abc@gmail.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    //alert("success"); "/order.json  if we arre using fire base we have to add json in the end"
+    axiousInstance
+      .post("/orders.json", order)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   render() {
     this.disabledInfo = { ...this.state.ingredients };
