d9652dec32340c22a6e07fa96e0a078ddbf620a2
sahithya14 <sahithyareddy33@gmail.com>
2020-08-19 21:46:09 +0530

    shouldComponentUpdate  lifecycle method added to Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "../Modal/Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux";
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clickedBackdrop={props.closeModal} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
-
-export default modal;
+export default class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.show !== this.props.show) {
+      console.log("props changed");
+      return true;
+    } else {
+      console.log("props are not changed");
+      return false;
+    }
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          clickedBackdrop={this.props.closeModal}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
