fc126bac104f6fc2e101b3f1661df4f33b65d999
sahithya14 <sahithyareddy33@gmail.com>
2020-09-09 01:48:53 +0530

    passes ingredients and added spinner in contact data ..orders are stored in server

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from "../../Checkout/ContactData/ContactData.module.css";
 import axiousInstance from "../../../hoc/axios-orders";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 class ContactData extends Component {
   state = {
     name: "",
@@ -11,21 +12,22 @@ class ContactData extends Component {
       pincode: ""
     }
   };
-  orderHandler = () => {
+  orderHandler = (event) => {
+    event.preventDefault();
     this.setState({ loading: true });
     var order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
       customer: {
-        name1: "sahithya",
+        name1: "sahithya14",
         address: {
-          street: "kukatpally",
-          pinCode: "505474",
-          country: "INDIA"
+          street: "kukatpally14",
+          pinCode: "50547414",
+          country: "INDIA143"
         },
-        email: "abc@gmail.com"
+        email: "abc14@gmail.com"
       },
-      deliveryMethod: "fastest"
+      deliveryMethod: "fastest14"
     };
     //alert("success"); "/order.json  if we arre using fire base we have to add json in the end"
     axiousInstance
@@ -40,38 +42,44 @@ class ContactData extends Component {
       });
   };
   render() {
+    var form = (
+      <form>
+        <input
+          className={classes.Inuput}
+          type="text"
+          name="Name"
+          placeholder="Your Name"
+        />
+        <input
+          className={classes.Inuput}
+          type="email"
+          name="Email"
+          placeholder="example@gmail.com"
+        />
+        <input
+          className={classes.Inuput}
+          type="text"
+          name="Street"
+          placeholder="Eg:GandhiNagar"
+        />
+        <input
+          className={classes.Inuput}
+          type="text"
+          name="pincode"
+          placeholder="Eg:505474"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.Contactdata}>
         <h4>Enter your Contact data </h4>
-        <form>
-          <input
-            className={classes.Inuput}
-            type="text"
-            name="Name"
-            placeholder="Your Name"
-          />
-          <input
-            className={classes.Inuput}
-            type="email"
-            name="Email"
-            placeholder="example@gmail.com"
-          />
-          <input
-            className={classes.Inuput}
-            type="text"
-            name="Street"
-            placeholder="Eg:GandhiNagar"
-          />
-          <input
-            className={classes.Inuput}
-            type="text"
-            name="pincode"
-            placeholder="Eg:505474"
-          />
-          <Button btnType="Success" clicked={this.orderHandler}>
-            ORDER
-          </Button>
-        </form>
+        {form}
       </div>
     );
   }
